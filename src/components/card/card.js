ad50251c71e168e4879840953af83bf85b8a4528
Mario Hernandez <mario.hernandez@mediacurrent.com>
2016-11-17 20:53:33 -0800

    Creates components demo theme

---
+++ src/components/card/card.js
@@ -0,0 +1,31 @@
+/**
+  * @file
+  * Attach behaviors for the card js.
+  */
+
+
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.card = {
+    attach: function (context, settings) {
+      var $bioButton = $('.card__view-bio', context);
+      var $bioDrawer = $('.card__bio', context);
+      var showBio = 'View full biography';
+      var hideBio = 'Hide biography';
+
+      $bioButton.on('click', function() {
+        $bioDrawer.toggleClass('is-open');
+        $bioButton.toggleClass('flip-carat');
+        if ($bioDrawer.hasClass('is-open')) {
+          $bioButton.text(hideBio);
+          $bioButton.toggleClass('is-open');
+        }
+        else {
+          $bioButton.text(showBio);
+          $bioButton.removeClass('is-open');
+        }
+      });
+    }
+  };
+})(jQuery);
